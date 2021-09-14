cabed662779af8695b8f1fd86737c8775bb9d641
Archana Malviya <89104298+Archanamalviya@users.noreply.github.com>
2021-09-14 03:55:27 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+
+import "./App.css";
+
+import Ptcf2 from "./Component/Ptcf2";
+import Ctcf2 from "./Component/Ctcf2";
+
+function App() {
+  return (
+    <div className="App">
+ 
+  <Ptcf2/>
+  <Ctcf2/>
+    </div>
+  );
+}
+export default App;
+
